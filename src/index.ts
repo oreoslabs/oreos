804c281243e6e08e15ecb1ddb6782e094176bdac
hairtail <zhaoxinlong@web3.com>
2023-01-22 20:07:33 +0800

    Re-export utils

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export { RpcService, RpcTcpClient, RpcTlsClient } from './rpcProvider';
+export {
+  RpcService,
+  RpcTcpClient,
+  RpcTlsClient,
+  SocketRpcResponseSchema,
+} from './rpcProvider';
+export { YupUtils, BufferUtils } from './utils';
 export {
   Account,
   createAccount,
